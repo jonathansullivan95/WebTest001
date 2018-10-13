32016962686625f0db1636fe1db47e776a94f5d6
Jonathan Sullivan <sullivanj@thehutgroup.com>
2018-10-13 23:40:49 +0100

    Updated file names

--- src/msg.js
+++ src/msg.js
@@ -1,5 +1,5 @@
 //- Using an anonymous function:
 function myFunction() {
-    document.getElementById("mainP").innerHTML = "CRACK M8";
+    document.getElementById("mainP").innerHTML = "lol";
     console.log("CLICKED BUTTON");
 }
